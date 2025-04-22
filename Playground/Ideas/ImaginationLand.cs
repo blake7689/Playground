fb399a2195baa03742f3f18a01ef1474ea1b8f08
blake7689 <blakec7689@gmail.com>
2025-04-22 02:39:06 +0100

    Added section for ideas. Started on image sort

---
+++ Playground/Ideas/ImaginationLand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace Playground.Ideas
+{
+    public class ImaginationLand
+    {
+        public static void ImageSort()
+        {
+            string[] fileEntries = Directory.GetFiles("Z:\\Pictures\\phone 1-16-2017");
+            foreach (string path in fileEntries)
+            {
+                DateTime first = File.GetCreationTime(path);
+                DateTime last = File.GetLastWriteTime(path);
+                FileInfo info = new FileInfo(path);
+                if (first != last)
+                {
+                    info.CreationTime = last;
+                }
+
+                Console.WriteLine($"File : {Path.GetFileName(path)}");
+                Console.WriteLine($"Creation Date : {info.CreationTime}");
+
+                //todo///////////////////////////
+                //sort by file type and date
+                //create folder structure
+                //remove duplicates
+                /////////////////////////////////
+            }
+        }
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Playground.Ideas;
+using System;
 
 public class Program
 {
     static void Main()
     {
+        ImaginationLand.ImageSort();
     }
 }
