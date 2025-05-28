3c88be78e1946544e238b8f8033455171638aa42
blake7689 <blakec7689@gmail.com>
2025-05-28 17:40:07 +0100

    class inheritance practice

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Playground;
 using Playground.Ideas;
 using System;
+using System.Runtime.CompilerServices;
+using static Program;
 
 public class Program
 {
+
     static void Main()
     {
         //ImaginationLand.ImageSort();
@@ -33,5 +36,40 @@ public class Program
         //train.AttachWagonFromLeft(13);
         //Console.WriteLine(train.DetachWagonFromRight()); // 7 
         //Console.WriteLine(train.DetachWagonFromLeft()); // 13
+
+        //TextInput input = new NumericInput();
+        //input.Add('1');
+        //input.Add('a');
+        //input.Add('0');
+        //Console.WriteLine(input.GetValue());
     }
+
+    //public class TextInput
+    //{
+    //    public string text;
+
+    //    public TextInput()
+    //    {
+    //        text = "";
+    //    }
+
+    //    public virtual void Add(char c)
+    //    {
+    //        text += c;
+    //    }
+
+    //    public string GetValue()
+    //    {
+    //        return this.text;
+    //    }
+    //}
+
+    //public class NumericInput : TextInput
+    //{
+    //    public override void Add(char c)
+    //    {
+    //        if (Char.IsNumber(c))
+    //            text += c;
+    //    }
+    //}
 }
